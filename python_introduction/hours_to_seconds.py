fb079a8ef0059ca7bd142401258e766545e7d4b1
Emmanuel Adomako <Adomakoemmanuel046@gmail.com>
2025-09-14 22:27:39 +0000

    seconds corrected

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,4 +1,4 @@
 hours = 2 #hours variable is assigned with value of 2
-seconds = (60 * 60) * 2 #seconds variable defined
+seconds = hours * 3600 #seconds variable defined
 print("2 hour(s) is", seconds , "seconds.") #printing output
 #work completed 
