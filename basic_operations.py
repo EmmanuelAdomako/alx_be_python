fdb05d59fa579a066ba0381001b39ad62e52be3c
Emmanuel Adomako <Adomakoemmanuel046@gmail.com>
2025-09-14 00:44:06 -0500

    Basic arithmetic

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+#defining variables
+number1 = 10
+number2 = 5
+#performing addition 
+addition_results = number1 + number2
+#performing subtraction
+subtraction_results = number1 - number2
+#performing multiplication
+multiplication_results = number1 * number2
+#printing output of computations 
+print("Addition of 10 and 5 is ", addition_results)
+print("Subtraction of 10 and 5 is ",  subtraction_results)
+print("Multiplication of 10 and 5 is ", multiplication_results)
