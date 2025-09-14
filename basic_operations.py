a096f4e353ebc216048b1eb8650edb50461d1b7f
Emmanuel Adomako <Adomakoemmanuel046@gmail.com>
2025-09-14 01:32:31 -0500

    new

--- basic_operations.py
+++ basic_operations.py
@@ -1,10 +1,7 @@
-#defining variables
-number1 = 10
-number2 = 5
-#performing addition 
-addition_results = number1 + number2
-#performing subtraction
-subtraction_results = number1 - number2
+number1 = 10 #variable number1 is assigned with value 10
+number2 = 5  #variable number2 is assigned with value 5
+addition_results = number1 + number2 #addition of number1 and number2 , should output 15
+subtraction_results = number1 - number2 #subtracting number2 from number1 , should output 5
 #performing multiplication
 multiplication_results = number1 * number2
 #printing output of computations 
---
+++ future_age_calculator.py
@@ -0,0 +1,2 @@
+age = input("How old are you?") #requesting user age and storing in age variable
+print("In 2050, you will be", int(age) + 27 , "years old.")
