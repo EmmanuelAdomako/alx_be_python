7b88587c084a56fa48e509b45ef2b73f074fae7d
Emmanuel Adomako <Adomakoemmanuel046@gmail.com>
2025-09-14 00:54:35 -0500

    Simple interest

---
+++ simple_interest.py
@@ -0,0 +1,8 @@
+#defining variables 
+principal = 1000
+rate = 0.05
+time = 3
+#calculating simple interest
+interest = principal * rate * time
+#printing the interest rate 
+print("The simple interest is:", interest)
